99b67246e83d779b506edebb6880a189c3ed12fd
adityarjadhav <adityarjadhav@outlook.com>
2023-12-25 22:44:07 +0530

    Guessing_game

---
+++ guess_game/src/main.rs
@@ -0,0 +1,30 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    
+    println!("Guess the number: ");
+
+        let mut guess = String::new();
+
+    io::stdin()
+
+        .read_line(&mut guess)
+        .expect("Error while reading the number");
+
+    let guess: i32 = guess.trim().parse().expect("Failed to parse the string");
+
+    match guess.cmp(&secret_number) {
+
+        Ordering::Less => println!("Secret number is greater than the guessed"),
+        Ordering::Greater => println!("Secret number is lesser than the guessed"),
+        Ordering::Equal => {
+            println!("You guessed the correct number!");
+            break;
+        }
+    }
+
+}
+
